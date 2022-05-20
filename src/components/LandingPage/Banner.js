df66c95945a27e8ed8883cd6df168d8d627bb315
SebasttianM <sebasttianvmarin@gmail.com>
2022-05-20 00:45:58 -0500

    Search bar tags

--- src/components/LandingPage/Banner.js
+++ src/components/LandingPage/Banner.js
@@ -10,16 +10,24 @@ const Banner = () => {
                   Find The Perfect Place to Live With your Family
               </h1>
           </div>
+          <section className='w-2/4 flex mt-20 justify-center self-end'>
+              <div>
+                  <tag className='active:bg-caribbean-green-400 px-7 pt-5 pb-4 m-1 hover:bg-caribbean-green-400 hover:text-white
+                  cursor-pointer select-text rounded-t-lg font-normal text-2xl'>Sell</tag>
+                  <tag className='px-7 pt-5 pb-4 m-1 hover:bg-caribbean-green-400 hover:text-white
+                  cursor-pointer select-text rounded-t-lg font-normal text-2xl'>Buy</tag>
+                  <tag className='px-7 pt-5 pb-4 m-1 hover:bg-caribbean-green-400 hover:text-white
+                  cursor-pointer select-text rounded-t-lg font-normal text-2xl'>Rent</tag>
+              </div>
+              {/* <div className=' flex flex-col border-2 border-red-500'>
+                  <input placeholder='Enter a word'/>
+                  <select className='text-black' placeholder='Select a location'>select your</select>
+              </div> */}
+          </section>
           <img className='z-0 '
               src="https://res.cloudinary.com/svartblood/image/upload/v1652812010/realmod/cars_snhxjw.png"
               alt=""
           />
-          <section>
-              <div className='flex flex-col'>
-                  <select className='text-black'>select your</select>
-                  <input className='text-black'></input>
-              </div>
-          </section>
       </div>
   );
 }
